af1dc12fc484b03057a83135fbc84c54d492681c
larimarit <larimaritgroup.ru@gmail.com>
2021-03-10 15:15:45 +0300

    (+-) sql

--- main/sql/menu.sql
+++ main/sql/menu.sql
@@ -1,10 +1,11 @@
-INSERT INTO "menu_menuitem" VALUES
- (3,70,'/accounts/logout/','Выход',0,1,0),
- (5,60,'/accounts/profile0/','Мой профиль',0,1,0),
- (6,10,'/accounts/login/','Вход',0,2,0),
- (7,20,'/accounts/register/','Регистрация',0,2,0),
- (8,30,'/finance/','Финансы',0,1,0),
- (9,20,'/companies/companies_page/0/companies','Организации',0,1,0),
- (10,5,'/site/vacancies_page/','Вакансии',0,2,0),
- (11,2,'/','Главная',0,2,0),
- (12,50,'/crm/clients_page0/','Клиенты',0,1,1);
+/*INSERT INTO "menu_menuitem" ("id","order","link_url","title","anonymous_only","menu_id","login_required") VALUES (1,10,'/main/','Главная',0,1,0);
+INSERT INTO "menu_menuitem" ("id","order","link_url","title","anonymous_only","menu_id","login_required") VALUES (2,40,'/projects/projects_page0/','Проекты',0,1,0);*/
+INSERT INTO "menu_menuitem" ("id","order","link_url","title","anonymous_only","menu_id","login_required") VALUES (3,70,'/accounts/logout/','Выход',0,1,0);
+INSERT INTO "menu_menuitem" ("id","order","link_url","title","anonymous_only","menu_id","login_required") VALUES (5,60,'/accounts/profile0/','Мой профиль',0,1,0);
+INSERT INTO "menu_menuitem" ("id","order","link_url","title","anonymous_only","menu_id","login_required") VALUES (6,10,'/accounts/login/','Вход',0,2,0);
+INSERT INTO "menu_menuitem" ("id","order","link_url","title","anonymous_only","menu_id","login_required") VALUES (7,20,'/accounts/register/','Регистрация',0,2,0);
+INSERT INTO "menu_menuitem" ("id","order","link_url","title","anonymous_only","menu_id","login_required") VALUES (8,30,'/finance/','Финансы',0,1,0);
+INSERT INTO "menu_menuitem" ("id","order","link_url","title","anonymous_only","menu_id","login_required") VALUES (9,20,'/companies/companies_page/0/companies','Организации',0,1,0);
+INSERT INTO "menu_menuitem" ("id","order","link_url","title","anonymous_only","menu_id","login_required") VALUES (10,5,'/site/vacancies_page/','Вакансии',0,2,0);
+INSERT INTO "menu_menuitem" ("id","order","link_url","title","anonymous_only","menu_id","login_required") VALUES (11,2,'/','Главная',0,2,0);
+INSERT INTO "menu_menuitem" ("id","order","link_url","title","anonymous_only","menu_id","login_required") VALUES (12,50,'/crm/clients_page0/','Клиенты',0,1,1);
