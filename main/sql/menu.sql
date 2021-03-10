2606e980b2ead8b007aec55bb6c717bea20d254b
larimarit <larimaritgroup.ru@gmail.com>
2021-03-10 15:09:05 +0300

    (+-) sql

--- main/sql/menu.sql
+++ main/sql/menu.sql
@@ -1,1 +1,12 @@
-INSERT INTO menu_menuitem (`order`, `link_url`, `title`, `anonymous_only`, `menu_id`, `login_required`) VALUES ('40', '/projects/projects_page0/', 'Проекты', '0', '1', '0');
+INSERT INTO "menu_menuitem" VALUES 
+ /*(1,10,'/main/','Главная',0,1,0),
+ (2,40,'/projects/projects_page0/','Проекты',0,1,0),*/
+ (3,70,'/accounts/logout/','Выход',0,1,0),
+ (5,60,'/accounts/profile0/','Мой профиль',0,1,0),
+ (6,10,'/accounts/login/','Вход',0,2,0),
+ (7,20,'/accounts/register/','Регистрация',0,2,0),
+ (8,30,'/finance/','Финансы',0,1,0),
+ (9,20,'/companies/companies_page/0/companies','Организации',0,1,0),
+ (10,5,'/site/vacancies_page/','Вакансии',0,2,0),
+ (11,2,'/','Главная',0,2,0),
+ (12,50,'/crm/clients_page0/','Клиенты',0,1,1);
