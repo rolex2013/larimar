9edc7b58aa6faf752354e6e20dc7ba3e520e0ef7
larimarit <larimaritgroup.ru@gmail.com>
2021-03-10 15:23:59 +0300

    (+-) sql

--- main/sql/menu.sql
+++ main/sql/menu.sql
@@ -1,5 +1,8 @@
-/*INSERT INTO menu_menuitem (`id`,`order`,`link_url`,`title`,`anonymous_only`,`menu_id`,`login_required`) VALUES (1,10,'/main/','Главная',0,1,0);
-INSERT INTO menu_menuitem (`id`,`order`,`link_url`,`title`,`anonymous_only`,`menu_id`,`login_required`) VALUES (2,40,'/projects/projects_page0/','Проекты',0,1,0);*/
+INSERT INTO menu_menu (`id`,`name`,`slug`,`base_url`,`description`) VALUES (1,'Главное меню','main_menu','/base.html','Меню после авторизации');
+INSERT INTO menu_menu (`id`,`name`,`slug`,`base_url`,`description`) VALUES (2,'Вход и Регистрация','logout_menu','/accounts/logout/','');
+
+INSERT INTO menu_menuitem (`id`,`order`,`link_url`,`title`,`anonymous_only`,`menu_id`,`login_required`) VALUES (1,10,'/main/','Главная',0,1,0);
+INSERT INTO menu_menuitem (`id`,`order`,`link_url`,`title`,`anonymous_only`,`menu_id`,`login_required`) VALUES (2,40,'/projects/projects_page0/','Проекты',0,1,0);
 INSERT INTO menu_menuitem (`id`,`order`,`link_url`,`title`,`anonymous_only`,`menu_id`,`login_required`) VALUES (3,70,'/accounts/logout/','Выход',0,1,0);
 INSERT INTO menu_menuitem (`id`,`order`,`link_url`,`title`,`anonymous_only`,`menu_id`,`login_required`) VALUES (5,60,'/accounts/profile0/','Мой профиль',0,1,0);
 INSERT INTO menu_menuitem (`id`,`order`,`link_url`,`title`,`anonymous_only`,`menu_id`,`login_required`) VALUES (6,10,'/accounts/login/','Вход',0,2,0);
