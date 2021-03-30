34fbed4149441f60a2fb3ad68165b55da3aae96c
larimarit <larimaritgroup.ru@gmail.com>
2021-03-30 14:58:42 +0300

    (+) Вход без прав на Меню (component_id==1). Залито с ноута...

--- main/templatetags/main_tags.py
+++ main/templatetags/main_tags.py
@@ -14,7 +14,7 @@ def left_menu(context, menuid=0, is_auth=False):
     if is_auth == True:
        #group = UserCompanyComponentGroup.objects.get(user_id=context.request.user, company_id=, component_id='1').order_by('-group_id')
        if menuid == 0:
-          nodes = MenuItem.objects.filter(Q(menu_id=2) & Q(is_active=True) & (Q(component_id__in=context.request.session['_auth_user_component_id']) | Q(component_id__in=[])))   # Главное (левое) меню
+          nodes = MenuItem.objects.filter(Q(menu_id=2) & Q(is_active=True) & (Q(component_id__in=context.request.session['_auth_user_component_id']) | Q(component_id__in=[]) | Q(component_id=1)))   # Главное (левое) меню
        else:
           nodes = MenuItem.objects.filter(Q(menu_id=menuid) & Q(is_active=True) & (Q(component_id__in=context.request.session['_auth_user_component_id']) | Q(component_id=None)))  
     else:
@@ -25,10 +25,12 @@ def left_menu(context, menuid=0, is_auth=False):
 def group_name(context, is_auth=False):
     groupname = ''
     if is_auth == True:
-       uccg = UserCompanyComponentGroup.objects.filter(user_id=context.request.user, component_id='1').order_by('-group_id')[0]
-       groupname = uccg.group.name
+       try:
+          uccg = UserCompanyComponentGroup.objects.filter(user_id=context.request.user, component_id='1').order_by('-group_id')[0]
+          groupname = '"'+uccg.group.name+'"'
+       except:
+          groupname = '"Не назначено"'         
     return (groupname)
-
 
 @register.inclusion_tag('left_menu.html', takes_context=True)
 def left__menu(context):
