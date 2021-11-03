ec45865ed75ec3f903b4d0c1f51fb641eac662e3
larimarit <larimaritgroup.ru@gmail.com>
2021-11-03 18:30:24 +0300

    (+-) API. Проверка приёма запроса на продакшене

--- larimar/settings.py
+++ larimar/settings.py
@@ -71,7 +71,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 10
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 10,
 }
 
 #CORS_ORIGIN_WHITELIST = ('localhost:8000',)
