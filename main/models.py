fa16796774634ce4ba6881253ebe2b7e58232025
Harry Larimarry <larimaritgroup.ru@gmail.com>
2023-12-20 13:47:15 +0300

    (+-) Localization

--- main/models.py
+++ main/models.py
@@ -10,9 +10,10 @@ from main.utils_lang import TranslateFieldMixin
 # request, пробрасываемый сюда из main\request_exposer.py
 exposed_request = ''
 
+
 class Meta_Param(TranslateFieldMixin, models.Model):
-    name_ru = models.CharField(_("Наименование"), max_length=64)
-    name_en = models.CharField(_("Наименование"), max_length=64, blank=True, null=True)
+    name_ru = models.CharField(_("Наименование_ru"), max_length=64)
+    name_en = models.CharField(_("Наименование_en"), max_length=64, blank=True, null=True)
     datecreate = models.DateTimeField(_("Дата"), auto_now_add=True)
     # *** если is_service==True, то доступна регистрация Организаций пользователями и тестирование Системы в мультипользовательском режиме ***
     is_service = models.BooleanField(_("Система, как сервис"), default=True)
@@ -23,14 +24,16 @@ class Meta_Param(TranslateFieldMixin, models.Model):
         return self.trans_field(exposed_request, 'name')
 
     class Meta:
-        ordering = ('datecreate',)     
+        ordering = ('datecreate',)
+
     def __str__(self):
         return (self.datecreate.strftime('%d.%m.%Y %H:%M:%S') + '|' + self.name)      
 
+
 class Meta_ObjectType(TranslateFieldMixin, models.Model):
     shortname = models.CharField(_("Код"), max_length=16)
-    name_ru = models.CharField(_("Наименование"), max_length=64)
-    name_en = models.CharField(_("Наименование"), max_length=64, blank=True, null=True)
+    name_ru = models.CharField(_("Наименование_ru"), max_length=64)
+    name_en = models.CharField(_("Наименование_en"), max_length=64, blank=True, null=True)
     tablename = models.CharField(_("Таблица"), max_length=64)
     sort = models.PositiveSmallIntegerField(_("Сортировка"), default=1, blank=True, null=True)
     is_active = models.BooleanField(_("Активность"), default=True)
@@ -43,12 +46,14 @@ class Meta_ObjectType(TranslateFieldMixin, models.Model):
         ordering = ('sort',)
         verbose_name = _('Список типов объектов')
         verbose_name_plural = _('Списки типов объектов')
+
     def __str__(self):
         return (self.name)
 
+
 class Dict_ProtocolType(TranslateFieldMixin, models.Model):
-    name_ru = models.CharField(_("Наименование"), max_length=64)
-    name_en = models.CharField(_("Наименование"), max_length=64, blank=True, null=True)
+    name_ru = models.CharField(_("Наименование_ru"), max_length=64)
+    name_en = models.CharField(_("Наименование_en"), max_length=64, blank=True, null=True)
     sort = models.PositiveSmallIntegerField(_("Сортировка"), default=1, blank=True, null=True)
     is_active = models.BooleanField(_("Активность"), default=True)
 
@@ -60,20 +65,20 @@ class Dict_ProtocolType(TranslateFieldMixin, models.Model):
         ordering = ('sort',)
         verbose_name = _('Протокол оповещения')
         verbose_name_plural = _('Протоколы оповещений')
+        
     def __str__(self):
         return (self.name)
 
 
 class Component(TranslateFieldMixin, MPTTModel):
-    parent = TreeForeignKey('self', null=True, blank=True, limit_choices_to={'is_active':True}, on_delete=models.CASCADE, related_name='component_children', verbose_name="Головной компонент")
-    # name = models.CharField(_("Наименование"), max_length=64, blank=True, null=True)
-    name_ru = models.CharField(_("Наименование"), max_length=64, blank=True, null=True)
-    name_en = models.CharField(_("Наименование"), max_length=64, blank=True, null=True)
+    parent = TreeForeignKey('self', null=True, blank=True, limit_choices_to={'is_active':True}, on_delete=models.CASCADE, related_name='component_children', verbose_name=_("Головной компонент"))
+    name_ru = models.CharField(_("Наименование_ru"), max_length=64, blank=True, null=True)
+    name_en = models.CharField(_("Наименование_en"), max_length=64, blank=True, null=True)
     code = models.CharField(_("Код"), max_length=64)
-    description_ru = models.CharField(_("Описание"), max_length=256, blank=True, null=True)
-    description_en = models.CharField(_("Описание"), max_length=256, blank=True, null=True)
-    menu_ru = models.CharField(_("Пункт меню"), max_length=256, blank=True, null=True)
-    menu_en = models.CharField(_("Пункт меню"), max_length=256, blank=True, null=True)
+    description_ru = models.CharField(_("Описание_ru"), max_length=256, blank=True, null=True)
+    description_en = models.CharField(_("Описание_en"), max_length=256, blank=True, null=True)
+    menu_ru = models.CharField(_("Пункт меню_ru"), max_length=256, blank=True, null=True)
+    menu_en = models.CharField(_("Пункт меню_en"), max_length=256, blank=True, null=True)
     is_employee_default = models.BooleanField(_("Подключать сотруднику по-умолчанию"), default=False)
     is_client_default = models.BooleanField(_("Подключать клиенту по-умолчанию"), default=False)
     is_active = models.BooleanField(_("Активность"), default=True)
@@ -81,10 +86,11 @@ class Component(TranslateFieldMixin, MPTTModel):
     @property
     def name(self):
         return self.trans_field(exposed_request, 'name')
-        #return self.name_ru
+
     @property
     def menu(self):
         return self.trans_field(exposed_request, 'menu')
+
     @property
     def description(self):
         return self.trans_field(exposed_request, 'description')
@@ -94,21 +100,22 @@ class Component(TranslateFieldMixin, MPTTModel):
 
     class MPTTMeta:
         order_insertion_by = ['code']
+
     class Meta:
         verbose_name = _('Компонент')
         verbose_name_plural = _('Компоненты')
 
 
 class Notification(TranslateFieldMixin, models.Model):
-    objecttype = models.ForeignKey('Meta_ObjectType', limit_choices_to={'is_active':True}, on_delete=models.CASCADE, related_name='meta_objecttype', verbose_name="Тип Объекта")
+    objecttype = models.ForeignKey('Meta_ObjectType', limit_choices_to={'is_active':True}, on_delete=models.CASCADE, related_name='meta_objecttype', verbose_name=_("Тип Объекта"))
     objectid = models.PositiveIntegerField(_("ID объекта"), default=0)
     datecreate = models.DateTimeField(_("Дата"), auto_now_add=True)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name="Автор")
-    type = models.ForeignKey('Dict_ProtocolType', limit_choices_to={'is_active':True}, on_delete=models.CASCADE, related_name='protocol_type', verbose_name="Тип")
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name=_("Автор"))
+    type = models.ForeignKey('Dict_ProtocolType', limit_choices_to={'is_active':True}, on_delete=models.CASCADE, related_name='protocol_type', verbose_name=_("Тип"))
     sendfrom = models.CharField(_("От кого"), max_length=64, blank=True, null=True)
     theme = models.CharField(_("Тема"), max_length=256, blank=True, null=True)
     text = RichTextUploadingField(_("Текст"), max_length=1024)
-    recipient = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, null=True, related_name='notify_recipient', verbose_name="Получатель")     
+    recipient = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, null=True, related_name='notify_recipient', verbose_name=_("Получатель"))     
     sendto = models.CharField(_("Кому"), max_length=64, blank=True, null=True)
     datesent = models.DateTimeField(_("Момент отправки"), auto_now_add=False, blank=True, null=True)
     dateread = models.DateTimeField(_("Момент прочтения"), auto_now_add=False, blank=True, null=True)
@@ -124,6 +131,7 @@ class Notification(TranslateFieldMixin, models.Model):
         verbose_name = _('Оповещение')
         verbose_name_plural = _('Оповещения')
 
+
 class ModelLog(TranslateFieldMixin, models.Model):
     componentname = models.CharField(_("Компонент"), max_length=16)
     modelname = models.CharField(_("Модель"), max_length=64)
@@ -137,7 +145,6 @@ class ModelLog(TranslateFieldMixin, models.Model):
         return (self.componentname + '. ' + self.modelname + self.date.strftime('%d.%m.%Y, %H:%M') + ' (' + self.author.username + ')' + self.log)
 
     class Meta:
-        #unique_together = ('project', 'status', 'date', 'author')
         ordering = ('componentname', 'modelname', 'modelobjectid', 'date')
         verbose_name = _('История Объекта')
         verbose_name_plural = _('Истории Объектов')
@@ -165,19 +172,16 @@ class Menu(TranslateFieldMixin, models.Model):
         return (self.name)
 
     class Meta:
-        #ordering = 'sort'
         verbose_name = _('Список меню')
         verbose_name_plural = _('Списки меню')
 
 
 class MenuItem(TranslateFieldMixin, MPTTModel):
-    # title = models.CharField(_("Наименование"), max_length=128)
     title_ru = models.CharField(_("Наименование_ru"), default='title', max_length=128)
     title_en = models.CharField(_("Наименование_en"), default='title', max_length=128)
     menu = models.ForeignKey('Menu', on_delete=models.CASCADE, related_name='menuitem_menu', verbose_name=_("Меню"))
     component = models.ForeignKey('Component', null=True, blank=True, on_delete=models.CASCADE, related_name='menuitem_component',
                                   verbose_name=_("Компонент"))
-    # description = models.TextField("Описание", null=True, blank=True) 
     description_ru = models.TextField(_("Описание_ru"), null=True, blank=True)
     description_en = models.TextField(_("Описание_en"), null=True, blank=True)
     link_url = models.CharField("URL", max_length=128)
@@ -196,11 +200,11 @@ class MenuItem(TranslateFieldMixin, MPTTModel):
 
     def __str__(self):
         return (self.menu.name + ' ' + self.title)
+
     class MPTTMeta:
-        #order_insertion_by = ['title']
         order_insertion_by = ['sort']
+
     class Meta:
-        #ordering = ('menu', 'sort')
         verbose_name = _('Пункты меню')
         verbose_name_plural = _('Пункты меню')
 
@@ -208,12 +212,12 @@ class MenuItem(TranslateFieldMixin, MPTTModel):
 class Dict_Theme(TranslateFieldMixin, MPTTModel):
     parent = TreeForeignKey('self', null=True, blank=True, limit_choices_to={'is_active': True},
                             on_delete=models.CASCADE, related_name='theme_children',
-                            verbose_name="Головная тематика")
-    name_ru = models.CharField(_("Наименование"), max_length=64)
-    name_en = models.CharField(_("Наименование"), max_length=64, blank=True, null=True)
+                            verbose_name=_("Головная тематика"))
+    name_ru = models.CharField(_("Наименование_ru"), max_length=64)
+    name_en = models.CharField(_("Наименование_en"), max_length=64, blank=True, null=True)
     # description = RichTextUploadingField("Описание")
-    description_ru = models.CharField(_("Описание"), max_length=256, blank=True, null=True)
-    description_en = models.CharField(_("Описание"), max_length=256, blank=True, null=True)
+    description_ru = models.CharField(_("Описание_ru"), max_length=256, blank=True, null=True)
+    description_en = models.CharField(_("Описание_en"), max_length=256, blank=True, null=True)
     is_active = models.BooleanField(_("Активность"), default=True)
 
     @property
