3f3e8ab0ef4fcf6ace084c858a954e16c49b914b
Harry Larimarry <larimaritgroup.ru@gmail.com>
2024-02-09 17:37:25 +0300

    (+-) ... after...

--- larimar/settings.py
+++ larimar/settings.py
@@ -418,7 +418,7 @@ else:
         "default": {
             # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
             # "ENGINE": "django.db.backends.mysql",
-            'ENGINE': 'mysql.connector.django'
+            'ENGINE': 'mysql.connector.django',
             "OPTIONS": {
                 "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
                 "charset": "utf8mb4",
